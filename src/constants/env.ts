65c5245dcbcdb8bb582a527106d69af56cf9a29d
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-01 18:07:30 +0100

    Added logs constants

---
+++ src/constants/env.ts
@@ -0,0 +1,8 @@
+// ** Enviroment variables **
+
+export default {
+    JWT_SECRET: process.env.JWT_SECRET || "randomtoken",
+    DB: {
+        URI: `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@perseocluster.gakol.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
+    }
+}
