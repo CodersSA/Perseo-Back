f65e0a253d0be18c9cfdf02e1330bbc0d05cba21
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-02 00:44:14 +0100

    Sign up middleware

---
+++ src/middlewares/auth/signUp.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { DATA_MISSING } from "../../constants/errors";
+import { IRegister } from './../../interfaces/IRegister';
+
+export const verifySignUpRequest = (req: Request, res: Response, next: NextFunction) => {
+    const { user, education, work }: IRegister = req.body;
+
+    if (!user)
+        return res.status(400).json({ error: DATA_MISSING });
+
+    if (typeof user.email == 'undefined' || typeof user.password == 'undefined')
+        return res.status(400).json({ error: DATA_MISSING });
+
+    next();
+}
