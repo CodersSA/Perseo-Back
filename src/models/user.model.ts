d243a84389033f591f3207dbfcad2fbf1deaa26c
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-01 18:22:34 +0100

    User model added

---
+++ src/models/user.model.ts
@@ -0,0 +1,40 @@
+// ** User Model **
+
+import { model, Schema, Document } from 'mongoose';
+import { compareHash, encrypt } from '../helpers/bcrypt';
+
+export interface IUser extends Document {
+    _id: string,
+    email: string
+    password: string
+}
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.pre<IUser>('save', async function (next) {
+    const user = this;
+    if (!user.isModified("password")) return next();
+
+    // New user
+    const hash = await encrypt(user.password);
+    user.password = hash;
+
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password: string, hash: string): Promise<boolean> {
+    return await compareHash(password, hash);
+};
+
+export default model<IUser>("User", userSchema);
