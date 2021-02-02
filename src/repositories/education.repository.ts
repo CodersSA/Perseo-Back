1434c4000ca3acd703c7afa44635941b11c8f3c7
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-03 00:08:50 +0100

    added updated education in repository

--- src/repositories/education.repository.ts
+++ src/repositories/education.repository.ts
@@ -4,3 +4,7 @@ export const db_addEducation = async (education: IEducation): Promise<void> => {
     const newEducation = new Education(education)
     await newEducation.save()
 }
+
+export const db_updateEducation = async (education: IEducation) => {
+    await Education.findOneAndUpdate({_id: education._id}, education)
+}
