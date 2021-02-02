c373a6478981457b3989e701cef2d8c02e03e283
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-02 17:48:05 +0100

    Sign in route added

--- src/routes/api/auth/auth.routes.ts
+++ src/routes/api/auth/auth.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import { verifySignUpRequest } from '../../../middlewares/auth/signUp.middleware';
-import { signUp } from './../../../controllers/auth.controller';
+import { signUp, signIn } from '../../../controllers/auth.controller';
+import { verifySignInRequest } from '../../../middlewares/auth/signIn.middleware';
 
 const router = Router();
 
-// router.get("/special", passport.authenticate('jwt', { session: false }), (req: Request, res: Response) => {
-//     res.send("test")
-// });
 router.post("/signup", verifySignUpRequest, signUp)
-// router.post("/signin", signIn)
+router.post("/signin", verifySignInRequest, signIn)
 
 export default router
