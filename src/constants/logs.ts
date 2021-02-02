950a87dbc202f7435b21176e97e44fa249337407
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-03 00:06:47 +0100

    Added return logs

--- src/constants/logs.ts
+++ src/constants/logs.ts
@@ -3,9 +3,11 @@
 export const DATABASE_OPEN_LOG = "Mongo connection stablished";
 export const USER_CREATED = "User created successfully";
 export const WORK_CREATED = " Work experience added successfully";
+export const WORK_UPDATED = " Work experience updated";
 export const WORK_DELETED = " Work experience deleted";
 export const EDUCATION_CREATED = " Education data added successfully";
-export const EDUCATION_DELETED = " Education data deleted";
+export const EDUCATION_DELETED = " Education deleted";
+export const EDUCATION_UPDATED = " Education updated";
 export const COURSE_CREATED = " Course created successfully";
 export const COURSE_EDITED = " Course edited successfully";
 export const COURSE_DELETED = " Course deleted successfully";
