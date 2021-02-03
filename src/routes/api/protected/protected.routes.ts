2d88ffed1ed539d46da08799cb35442be6e266fd
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-03 01:30:44 +0100

    Added profile route

--- src/routes/api/protected/protected.routes.ts
+++ src/routes/api/protected/protected.routes.ts
@@ -3,6 +3,8 @@ import { addEducationToUser, updateEducation } from '../../../controllers/educat
 import { addWorkToUser, updateWorkToUser } from '../../../controllers/work.controller';
 import { verifyAddEducation, verifyUpdateEducation } from './../../../middlewares/education/addEducation.middleware';
 import { verifyAddWork, verifyUpdateWork } from './../../../middlewares/work/addWork.middleware';
+import { getProfile } from './../../../controllers/user.controller';
+import passport from 'passport';
 
 const router = Router();
 
@@ -10,5 +12,6 @@ router.post("/education", verifyAddEducation, addEducationToUser)
 router.patch("/education", verifyUpdateEducation, updateEducation)
 router.post("/work", verifyAddWork, addWorkToUser)
 router.patch("/work", verifyUpdateWork, updateWorkToUser)
+router.get("/profile", passport.authenticate('jwt', { session: false }), getProfile)
 
 export default router
