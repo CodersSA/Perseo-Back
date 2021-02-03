afdcb50689a533d9740d0d0bbe799bb71623082f
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-03 01:30:08 +0100

    Get works method added

--- src/repositories/work.repository.ts
+++ src/repositories/work.repository.ts
@@ -8,3 +8,7 @@ export const db_addWork = async (work: IWork): Promise<void> => {
 export const db_updateWork = async (work: IWork) => {
     await Work.findOneAndUpdate({ _id: work._id }, work)
 }
+
+export const db_getWorks = async (userID: string): Promise<IWork[]> => {
+    return Work.find({ userID: userID })
+}
